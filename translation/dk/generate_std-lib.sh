206912bd2e0186e86cf7035619f5fe2f6db0767f
Thiago <tcesar@telecom-paris.fr>
2021-07-06 16:57:05 +0200

    adds translations folder

---
+++ translation/dk/generate_std-lib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+AGDA_DIR=$1/src
+EXEC="$2"
+flags="$3 --outDir=$4"
+declare -i NB=$5
+
+i=0
+cd $AGDA_DIR
+find . -name "*.agda" | sort |
+    while read fil;
+    do
+        if (("$NB" == "$i"))
+        then
+            break
+        fi;
+        echo $i;
+        i=$(($i+1));
+        $EXEC $flags "$fil";
+    done
